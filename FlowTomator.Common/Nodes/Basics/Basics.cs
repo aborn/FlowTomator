ff7476e260aee2522bfcbb9e24a15940d95a67a2
aborn <loveaborn@foxmail.com>
2023-04-15 22:47:29 +0800

    添加结果

--- FlowTomator.Common/Nodes/Basics/Basics.cs
+++ FlowTomator.Common/Nodes/Basics/Basics.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
+using System.Runtime.Remoting.Contexts;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -21,10 +22,19 @@ namespace FlowTomator.Common
             }
         }
 
+        public override IEnumerable<Variable> Outputs
+        {
+            get
+            {
+                yield return content;
+            }
+        }
+
         private Variable<int> i = new Variable<int>("i", 0, "The path of the application to launch");
         private Variable<int> start = new Variable<int>("起始值", 0, "初始值");
         private Variable<int> end = new Variable<int>("结束值", 0, "初始值");
-        
+        private Variable content = new Variable("Content", typeof(object), null, "The content of the clipboard");
+
         public override NodeResult Run()
         {
             if (start.Value >= end.Value)
@@ -32,10 +42,13 @@ namespace FlowTomator.Common
                 return NodeResult.Skip;
             }
 
-            for (int i=start.Value; i<end.Value;i++)
+            for (int j=start.Value; j<end.Value;j++)
             {
-                Log.Debug("循环进行中，当前值 {0}", i);
+                Log.Debug("循环进行中，当前值 {0} {1}", j, i.Value);
+                i.Value = j * 2;
             }
+            content.Value = i.Value;
+            Log.Debug("结果为：{0}", content.Value);
             return NodeResult.Success;
         }
     }
